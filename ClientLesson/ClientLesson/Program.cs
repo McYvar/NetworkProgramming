f1fcd77cf26fd0ef8bdf596887db20fab42781b6
McYvar <yvarftoorop@hotmail.com>
2023-04-24 10:33:44 +0200

    Added git ignore and first connection test

---
+++ ClientLesson/ClientLesson/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPHostEntry ipHostInfo = Dns.GetHostEntry("annie.hku.nl");
+IPAddress ipAddress = ipHostInfo.AddressList[0];
+IPEndPoint ipEndPoint = new(ipAddress, 7331);
+
+using Socket client = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+await client.ConnectAsync(ipEndPoint);
+InputHandlerAsync();
+ReceiveHanlderAsync();
+
+async void InputHandlerAsync()
+{
+    while (true)
+    {
+        var message = Console.ReadLine();
+
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        _ = await client.SendAsync(messageBytes, SocketFlags.None);
+    }
+}
+
+async void ReceiveHanlderAsync()
+{
+
+    while (true)
+    {
+        var buffer = new byte[1_024];
+        var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+        var response = Encoding.UTF8.GetString(buffer, 0, received);
+        Console.WriteLine(response);
+    }
+}
+
+client.Shutdown(SocketShutdown.Both);
